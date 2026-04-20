1025c2bca1173fe384acb21c9d8b72328aeea977
priyanshiagarwall <priyanshiagarwalxc28@gmail.com>
2026-04-20 07:33:15 +0530

    Backend user

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from "express"
+import "dotenv/config"
+import cors from "cors"
+import http from "http"
+import { connectDB } from "./lib/db.js"
+import userRouter from "./routes/userRoutes.js"
+
+
+const app=express()
+const server=http.createServer(app)
+
+app.use(express.json({limit:"4mb"}))
+app.use(cors())
+
+app.use("/api/status", (req, res) => res.send("Server is live"));
+app.use("/api/status", userRouter);
+
+
+await connectDB();
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log("Server is running on PORT: " + PORT);
+});
+    // Export server for Vervel
+    export default server;
